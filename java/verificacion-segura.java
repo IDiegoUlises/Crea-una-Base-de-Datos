d31e327b99af436ee92dc6b58c06d8d96c580051
Diego Ulises <51557342+IDiegoUlises@users.noreply.github.com>
2020-01-24 21:21:13 -0300

    Create verificacion-segura.java

---
+++ java/verificacion-segura.java
@@ -0,0 +1,8 @@
+String mensaje = "verdadero"
+String comparar = "verdadero"
+        if(mensaje.equals(comparar))
+        {
+           //Envia los datos
+           output.writeUTF("devolviendo datos");
+           System.out.println("mensaje es igual a devolver");
+        }
